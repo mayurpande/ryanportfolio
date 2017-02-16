8301d17a7f9628e12471dbc589cb0aa09e2654e7
Mayur Pande <mayurpande.uk@gmail.com>
2017-02-16 16:38:42 +0000

    Added crossfade function to app.js

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+//$(document).ready(function(){
+    $('#port-ul').hide();
+    function crossfade(curr, next) {
+        $(curr).fadeOut(1200, function() {
+            $(next).fadeIn(1300);
+        });
+    }
+    
+
+
+
+    
+//});
+
+
+
+  
