6ba2907324bc98731a5e579cd3671c7d17c0cc86
Mayur Pande <mayurpande.uk@gmail.com>
2017-02-16 17:56:10 +0000

    Added misc-ul to hide list

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,21 @@
-//$(document).ready(function(){
+$(document).ready(function(){
     $('#port-ul').hide();
     $('#land-ul').hide();
-    function crossfade(curr, next) {
-        $(curr).fadeOut(1000, function() {
-            $(next).fadeIn(1300);
-        });
-    }
+    $('#misc-ul').hide();
+
+});
+
+function crossfade(curr, next) {
+    $(curr).fadeOut(1000, function() {
+        $(next).fadeIn(1300);
+    });    
+}
     
 
 
 
     
-//});
+
 
 
 
