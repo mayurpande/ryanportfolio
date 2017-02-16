7f89f61d7b1727b95809751156f0fd98f5805aa7
Mayur Pande <mayurpande.uk@gmail.com>
2017-02-16 16:43:11 +0000

    Hidden land-ul

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 //$(document).ready(function(){
     $('#port-ul').hide();
+    $('#land-ul').hide();
     function crossfade(curr, next) {
         $(curr).fadeOut(1200, function() {
             $(next).fadeIn(1300);
