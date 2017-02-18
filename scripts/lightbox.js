8bf7e268de1af6ca1850fb1e64506bb2f1df2cd2
Mayur Pande <mayurpande.uk@gmail.com>
2017-02-18 23:39:15 +0000

    Created lightbox, added overlay div on click of image

---
+++ scripts/lightbox.js
@@ -0,0 +1,50 @@
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+//An image to overlay
+$overlay.append($image);
+
+//A caption to overlay
+$overlay.append($caption);
+
+//Add overlay
+$("#wrapper").append($overlay);
+
+//Capture the click event on a link to an image
+$(".lightBox a").click(function(event){
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  //Update overlay with the image linked in the link
+  $image.attr("src", imageLocation);
+  
+  var t = $(this).parent();
+  if(t.attr("id") == "misc-ul"){
+      $('#misc-ul').hide();
+      $overlay.addClass('toggleMisc');
+  }
+  $('.footer').hide();
+  //Show the overlay.
+  $overlay.show();
+  
+  //Get child's alt attribute and set caption
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+
+//When overlay is clicked
+$overlay.click(function(){
+  
+  //console.log(t);
+  if($(this).hasClass('toggleMisc')){
+        $('#misc-ul').show();
+        $(this).removeClass('toggleMisc');
+
+  }
+  $('.footer').show();
+  //Hide the overlay
+  $overlay.hide();
+  
+
+ 
+});
