622863d82770e22303cc800767958efa94844c9d
Mayur Pande <mayurpande.uk@gmail.com>
2017-03-02 14:52:02 +0000

    Hidden uls that do not have id of home page

--- scripts/app.js
+++ scripts/app.js
@@ -2,21 +2,18 @@ $(document).ready(function(){
    // $('#port-ul').hide();
     //$('#land-ul').hide();
     //$('#misc-ul').hide();
+    $("ul").each(function(index){
+      if($(this).attr("id") != "home-ul"){
+        $(this).hide();
+      }
+      console.log("this worked");
+   });
+
 
 });
 
 function crossfade(curr, next) {
     $(curr).fadeOut(1000, function() {
         $(next).fadeIn(2000);
-    });    
+    });
 }
-    
-
-
-
-    
-
-
-
-
-  
