b564cf166a6b228cecfe030ae681fa9e175b384a
Mayur Pande <mayurpande.uk@gmail.com>
2017-03-03 13:54:51 +0000

    added code to crossfade function to check if closest element is div or ul and add and remove class tempActive

--- scripts/app.js
+++ scripts/app.js
@@ -12,4 +12,13 @@ function crossfade(curr, next) {
     $(curr).fadeOut(1000, function() {
         $(next).fadeIn(2000);
     });
+
+    if($(curr).closest('div')){
+      ($(curr).closest('div').removeClass('tempActive'));
+      ($(next).closest('ul').addClass('tempActive'));
+    }else{
+      ($(curr).closest('ul').removeClass('tempActive'));
+      ($(next).closest('ul')).addClass('tempActive');
+    }
+
 }
