73d48713104f9a3905cf7a312b5b24791fc5ac24
Mayur Pande <mayurpande.uk@gmail.com>
2017-03-17 15:16:43 +0000

    Cleaned up home controller removed unecessary code

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,9 +4,6 @@ namespace App\Controllers;
 
 //import model name
 use App\Models\Home_Page;
-use App\Models\Portrait;
-use App\Models\Landscape;
-use App\Models\Miscellaneous;
 
 //necessary because we are passing slim views instance
 use Slim\Views\Twig as View;
@@ -19,45 +16,14 @@ class HomeController extends Controller{
 	//we now have access to our whole container because we have the container in our base controller
 		$homePage = Home_Page::all();
 
-			foreach($homePage as $id){
-				$homeItem = $id->id;
-				//$homeItem = $id['ul_update_no'];
+		foreach($homePage as $id){
+			$homeItem = $id->id;
+		}
 
-			}
+		return $this->view->render($response, 'home.twig', [
+        'homePage' => $homePage,
+        'homeItem' => $homeItem
 
-
-
-
-
-        $portPage = Portrait::all();
-        foreach($portPage as $ids){
-            $portItem = $ids->ids;
-        }
-
-        $landPage = Landscape::all();
-        foreach($landPage as $idss){
-            $landItem = $idss->idss;
-        }
-
-        $miscPage = Miscellaneous::all();
-        foreach($miscPage as $idsss){
-            $miscItem = $idsss->idsss;
-        }
-
-
-
-
-
-        return $this->view->render($response, 'home.twig', [
-            'homePage' => $homePage,
-            'homeItem' => $homeItem,
-            'portPage' => $portPage,
-            'portItem' => $portItem,
-            'landPage' => $landPage,
-            'landItem' => $landItem,
-            'miscPage' => $miscPage,
-            'miscItem' => $miscItem
-
-        ]);
+    ]);
 	}
 }
