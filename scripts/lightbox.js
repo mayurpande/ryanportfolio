f11266d26489c173bc3948a7c7d6e8f31c63be0a
Mayur Pande <mayurpande.uk@gmail.com>
2017-03-17 10:31:51 +0000

    Appended button to lightbox

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,6 +1,10 @@
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
 var $caption = $('<p></p>');
+var $nextButton = $('<button>Next</button>');
+var $imgTest = $('<img>');
+var x;
+
 
 //An image to overlay
 $overlay.append($image);
@@ -8,46 +12,70 @@ $overlay.append($image);
 //A caption to overlay
 $overlay.append($caption);
 
+$overlay.append($nextButton);
 //Add overlay
 $("#wrapper").append($overlay);
 
 //Capture the click event on a link to an image
 $(".lightBox a").click(function(event){
   event.preventDefault();
+  x = this;
+  getNextImage(x);
+
+
   var imageLocation = $(this).attr("href");
+
   //Update overlay with the image linked in the link
   $image.attr("src", imageLocation);
 
-  var miscUl = $(this).parent();
-  if(miscUl.attr("id") == "misc-ul"){
-      $('#misc-ul').hide();
-      $overlay.addClass('toggleMisc');
-  }
+
   $('.footer').hide();
   //Show the overlay.
   $overlay.show();
 
+
   //get p element text and add it to caption
-  var captionText = $(this).children("p").text();
-  $caption.text(captionText);
-
-
+/*  var captionText = $(this).children("p").text();
+  $caption.text(captionText);.*/
 
 });
 
+
+
+
+
+
 //When overlay is clicked
-$overlay.click(function(){
+/*$overlay.click(function(){
 
-  //console.log(t);
-  if($(this).hasClass('toggleMisc')){
-        $('#misc-ul').show();
-        $(this).removeClass('toggleMisc');
-
-  }
   $('.footer').show();
   //Hide the overlay
   $overlay.hide();
+});*/
 
 
 
-});
+function getNextImage(x){
+  //console.log(currImage);
+
+
+  var t = $(x).closest('li');
+
+  var nextLi = t.closest('li').nextAll('.temp').slideToggle();
+  //console.log(nextLi);
+  /*$(nextLi).each(function(){
+     arr.push($(this).children().attr("href"));
+  });*/
+  $nextButton.click(function(){
+
+
+    $(nextLi).each(function(){
+
+       console.log($(this).children().attr("href"));
+       $image.attr("src",$(this).children().attr("href"));
+       $overlay.append($image);
+       $overlay.show();
+    });
+  });
+
+}
