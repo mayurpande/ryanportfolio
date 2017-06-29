8de410a2e4da24497e2968d65035fb47f7c29425
Mayur Pande <mayurpande.uk@gmail.com>
2017-06-29 11:25:05 +0100

    Used Home_Page model, got data from db, passed to view

--- app/Controllers/ContactUsController.php
+++ app/Controllers/ContactUsController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controllers;
 
+//import model name
+use App\Models\Home_Page;
+
 //necessary because we are passing slim views instance
 use Slim\Views\Twig as View;
 
 class ContactUsController extends Controller{
-	
+
 
 	public function index($request, $response){
 	 //now we can use this view object and render the views
-	//we now have access to our whole container because we have the container in our base controller 
-		return $this->view->render($response, 'contact-us.twig');
+	//we now have access to our whole container because we have the container in our base controller
+
+		$homePage = Home_Page::all();
+
+		foreach($homePage as $id){
+			$homeItem = $id->id;
+		}
+		return $this->view->render($response, 'contact-us.twig'[
+        'homePage' => $homePage,
+        'homeItem' => $homeItem
+
+    ]);
 	}
 }
