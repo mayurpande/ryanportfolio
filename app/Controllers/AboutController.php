1401fa40cf1207aee03812d45ca28fb309280abb
Mayur Pande <mayurpande.uk@gmail.com>
2017-07-14 13:57:04 +0100

    used home_page model to retrieve data from db to pass to template

--- app/Controllers/AboutController.php
+++ app/Controllers/AboutController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 //import model name
 use App\Models\About_Page;
+use App\Models\Home_Page;
 
 //necessary because we are passing slim views instance
 use Slim\Views\Twig as View;
@@ -16,12 +17,18 @@ class AboutController extends Controller{
 	//we now have access to our whole container because we have the container in our base controller
 
 		$aboutPage = About_Page::all();
+		$homePage = Home_Page::all();
 
 		foreach($aboutPage as $id){
 			$aboutItem = $id->id;
 		}
+		foreach($homePage as $id){
+			$homeItem = $id->id;
+		}
 
 		return $this->view->render($response, 'about-us.twig',[
+				'tPage' => $homePage,
+				'tItem' => $homeItem,
 				'aboutPage' => $aboutPage,
 				'aboutItem' => $aboutItem
 
