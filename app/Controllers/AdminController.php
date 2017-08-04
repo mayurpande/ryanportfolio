0dceb505d67bf0910f778c86d3c5963861c32a37
Mayur Pande <mayurpande.uk@gmail.com>
2017-08-04 12:04:34 +0100

    created get and post methods for creating new landing page image

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -252,6 +252,29 @@ class AdminController extends Controller{
 
 		}
 
+		//create contact page Info
+		public function getNewLandingCreate($request,$response){
+			return $this->view->render($response,'admin-landing.twig');
+		}
+
+		public function postNewLandingCreate($request,$response){
+
+			$landing = Landing_Page::create([
+						'landing_img' => $request->getParam('landing_img')
+			]);
+
+			if ($landing) {
+							$this->flash->addMessage('success','You have added image ' . $landing->landing_img . ' to landing page gallery');
+							return $response->withRedirect($this->router->pathFor('admin.update'));
+			} else {
+							$this->flash->addMessage('error','You have not added image ' . $landing->landing_image . ' to landing page gallery');
+							return $response->withRedirect($this->router->pathFor('admin.update'));
+			}
+
+
+
+		 }
+
 
 
 
